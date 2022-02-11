4d303fbe7eb6e58913c8a56413a7e808e2e3bdad
Harendra Prajapati <harendraprajapati72@gmail.com>
2022-02-11 22:35:46 +0530

    Add files via upload

---
+++ Bus.java
@@ -0,0 +1,16 @@
+public class Bus {
+    public static void main(String[] args) {
+        int passangers=0;
+        passangers = passangers+9;
+        System.out.println(passangers);
+        passangers=passangers-5;
+        System.out.println(passangers);
+        
+        int passangers1=0;//remove code redundency
+        passangers1 += 9;
+        System.out.println(passangers1);
+        passangers1 -= 5;
+        System.out.println(passangers1);
+    }
+    
+}
