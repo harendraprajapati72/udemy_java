b52aaf6ed6357d638fd2b222ea9dcc0e3b5789fb
Harendra Prajapati <harendraprajapati72@gmail.com>
2022-02-11 22:36:23 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,9 @@
+public class Variable{
+public static void main(String[] args) {
+    int passanger=30;
+    System.out.println(passanger);
+
+    int busTickets=passanger;
+    System.out.println(busTickets);
+
+}}
