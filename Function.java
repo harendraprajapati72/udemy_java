dd237f7d3941c17a667ce5e27eb882266713621c
Harendra Prajapati <harendraprajapati72@gmail.com>
2022-02-24 01:23:23 +0530

    Add files via upload

---
+++ Function.java
@@ -0,0 +1,20 @@
+public class Function {// functions provide high reusebility of code and remove code redundency
+    
+    public static void main(String[] args) {
+        myFunction1();
+        harendra();
+        myFunction1();
+        harendra();
+    }
+    public static void myFunction1(){
+        System.out.println("hello i am harendra ");
+        System.out.println("how may i help you");
+    }
+    
+    public static void harendra(){
+        System.out.println("\nthis is all about me");
+    }
+
+}
+
+
