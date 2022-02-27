9d9f2a551165dd9f43504c5de03ce67d95e9ff8c
Harendra Prajapati <harendraprajapati72@gmail.com>
2022-02-28 00:53:11 +0530

    Add files via upload

---
+++ BultInFunction.java
@@ -0,0 +1,14 @@
+public class BultInFunction {
+    public static void main(String[] args) {
+
+        double sine=Math.sin(90);
+        System.out.println("The sin of 1.2 is : "+sine);
+
+        double power=Math.pow(2, 4);
+        System.out.println("2 to The power 4 is : "+power);
+
+        double random=Math.random()*100;
+        System.out.println("The random number is : "+random);
+    }
+    
+}
