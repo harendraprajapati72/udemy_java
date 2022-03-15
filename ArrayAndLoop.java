bc0d9926005c392c527296916cd8360e1718764b
Harendra Prajapati <harendraprajapati72@gmail.com>
2022-03-16 00:11:04 +0530

    Add files via upload

---
+++ ArrayAndLoop.java
@@ -0,0 +1,11 @@
+public class ArrayAndLoop {
+
+    public static void main(String[] args) {
+        String[] states ={"up","mp","bihar","gazipur"};
+       System.out.println("The no of elements is  " + states.length);
+        for (int i=0;i<4;i++){
+            System.out.println("The elements at index "+ i + " is "+states[i]);
+        }
+    }
+}
+  
