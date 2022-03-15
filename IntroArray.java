a5a7f839f0d9980d20ea3bd83e8a9925a972683e
Harendra Prajapati <harendraprajapati72@gmail.com>
2022-03-15 23:36:59 +0530

    Add files via upload

---
+++ IntroArray.java
@@ -0,0 +1,12 @@
+class IntroArray {
+    public static void main(String[] args) {
+        String[] States={"up","mp","vihar","gazipur"};
+        System.out.println(States[0]);  
+        System.out.println(States[1]);        
+        System.out.println(States[2]);        
+        System.out.println(States[3]); 
+        // System.out.println(States[4]); you can not access the element out of array range        
+       
+
+    }
+}
